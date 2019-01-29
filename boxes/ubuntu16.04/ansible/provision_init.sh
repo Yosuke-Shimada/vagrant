3c7bf7750e799ae5eadfb12869ccc0c8f2a76b27
madayo <madayodama@gmail.com>
2019-01-29 20:04:30 +0900

    プロビジョニング整理

--- boxes/ubuntu16.04/ansible/provision_init.sh
+++ boxes/ubuntu16.04/ansible/provision_init.sh
@@ -3,17 +3,11 @@
 # yum update all
 sudo apt-get update -y
 
-# dotfiles
 chmod 600 ~/.ssh/id_rsa
-cd ~
-if [ -e ./dotfiles ]; then
-  git clone git@github.com:chan-yo/dotfiles.git
-  /bin/bash ./bin/initialize/initialize.sh
-fi
 
 # ansible
-sudo apt-get install software-properties-common -y
-sudo apt-add-repository ppa:ansible/ansible -y
-sudo apt-get update -y
-sudo apt-get install ansible -y
-ansible-playbook -i 127.0.0.1 /vagrant/ansible/playbook.yml
+#sudo apt-get install software-properties-common -y
+#sudo apt-add-repository ppa:ansible/ansible -y
+#sudo apt-get update -y
+#sudo apt-get install ansible -y
+#ansible-playbook -i 127.0.0.1 /vagrant/ansible/playbook.yml
--- readme.md
+++ readme.md
@@ -1,10 +1,24 @@
+# 専用の鍵ファイル作成  
+```
+ssh-keygen -b 521 -t ecdsa -C 'only used by vagrant' -f ~/.ssh/id_ecdsa_vagrant
+```
+# 作業用vmの作成  
 ```sh
 cd boxes/
 cp -ar foo test_foo
 cd test_foo
 vi Vagrantfile
   edit: config.vm.hostname = "foo"
+  # アンダーバーは使用できないので注意
   edit: vb.name = "bar"
 bash ../../bin/initialize.sh
 ssh foo
 ```
+# 2回目以降の起動  
+```sh
+cd boxes/
+cd test_foo
+vagrant up
+```
+# dotfiles のインストール
+https://github.com/madayo/dotfiles
